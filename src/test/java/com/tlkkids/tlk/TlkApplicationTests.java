a5903b5d05364cd143e9c798c28fbb3b32d78a7c
Matheus Brenner <108211843+MathiasBelmont@users.noreply.github.com>
2025-03-05 17:02:50 -0300

    ADD: Primeiro commit

---
+++ src/test/java/com/tlkkids/tlk/TlkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlkkids.tlk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TlkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
