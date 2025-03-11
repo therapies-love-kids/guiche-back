2f489712f1fbd227249273af1fcadfc9e7292abe
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-03-11 10:06:18 -0300

    ADD: Configuração "UserController"

--- src/main/java/com/tlk/br/api/controllers/UserController.java
+++ src/main/java/com/tlk/br/api/controllers/UserController.java
@@ -1,5 +1,10 @@
 package com.tlk.br.api.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
 }
