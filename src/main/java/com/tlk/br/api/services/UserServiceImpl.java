8fb80aab843177588acc8223a49a7d9dfb6267c6
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-03-12 16:29:07 -0300

    MOD: Implement save method in UserServiceImpl to persist UserDTO

--- src/main/java/com/tlk/br/api/services/UserServiceImpl.java
+++ src/main/java/com/tlk/br/api/services/UserServiceImpl.java
@@ -2,15 +2,26 @@ package com.tlk.br.api.services;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.context.annotation.Bean;
+
 import com.tlk.br.api.domain.dtos.UserDTO;
 import com.tlk.br.api.domain.entitites.User;
+import com.tlk.br.api.repositories.UserRepository;
 
 public class UserServiceImpl implements UserService {
 
+    private final UserRepository UserRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.UserRepository = userRepository;
+    }
+
     @Override
     public User save(UserDTO UserDTO) {
-        // TODO Auto-generated method stub
-        return null;
+        User user = new User();
+        BeanUtils.copyProperties(UserDTO, user);
+        return UserRepository.save(user);
     }
 
     @Override
