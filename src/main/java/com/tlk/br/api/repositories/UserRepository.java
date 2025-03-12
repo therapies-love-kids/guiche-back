2c5b3a67284fa288d9869f9ed27ec593907a8d2b
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-03-12 15:44:19 -0300

    ADD: Arquivo "UserRepository.java"

--- src/main/java/com/tlk/br/api/repositories/UserRepository.java
+++ src/main/java/com/tlk/br/api/repositories/UserRepository.java
@@ -1,7 +1,8 @@
 package com.tlk.br.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.tlk.br.api.domain.entites.User;
+
+import com.tlk.br.api.domain.entitites.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
