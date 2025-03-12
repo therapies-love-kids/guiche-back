e74b4ef6f3344093399c9009e5f12e85ca38f03c
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-03-12 15:44:49 -0300

    MOD: Metodos em "UserService.java"

--- src/main/java/com/tlk/br/api/services/UserService.java
+++ src/main/java/com/tlk/br/api/services/UserService.java
@@ -1,5 +1,14 @@
 package com.tlk.br.api.services;
 
-public interface UserService {
+import com.tlk.br.api.domain.entitites.User;
+import com.tlk.br.api.domain.dtos.UserDTO;
 
+import java.util.*;
+
+public interface UserService {
+    User save(UserDTO userDTO);
+    List<User> findAll();
+    User findById(Long id);
+    User update(Long id,User UserDTO);
+    void delete(Long id);
 }
