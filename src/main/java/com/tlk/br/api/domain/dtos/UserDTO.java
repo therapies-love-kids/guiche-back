0abd87e31908d15cc487a52f1ef9157a8b3c9e65
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-03-12 16:28:49 -0300

    ADD: Implement UserDTO with fields for name and password

--- src/main/java/com/tlk/br/api/domain/dtos/UserDTO.java
+++ src/main/java/com/tlk/br/api/domain/dtos/UserDTO.java
@@ -1,5 +1,32 @@
 package com.tlk.br.api.domain.dtos;
 
 public class UserDTO {
-    
+
+    private String name;
+    private String password;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(Long id, String name, String password) {
+
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
