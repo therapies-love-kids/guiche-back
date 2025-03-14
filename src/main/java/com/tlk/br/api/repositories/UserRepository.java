c76b7cefb295a2ca5d83470c6578a6ff52716a5d
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-03-14 09:05:13 -0300

    ADD: Anotado UserRepository com @Repository e importados os pacotes necessários

--- src/main/java/com/tlk/br/api/repositories/UserRepository.java
+++ src/main/java/com/tlk/br/api/repositories/UserRepository.java
@@ -1,9 +1,12 @@
 package com.tlk.br.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.tlk.br.api.domain.entitites.User;
+import java.util.*;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
