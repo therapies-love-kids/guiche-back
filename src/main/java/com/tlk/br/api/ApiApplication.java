fbb3b98e28d69ec580f1a45189b9dff61cde9ee8
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-03-14 08:58:54 -0300

    ADD: Habilitado "JPA repositories" em "ApiApplication"

--- src/main/java/com/tlk/br/api/ApiApplication.java
+++ src/main/java/com/tlk/br/api/ApiApplication.java
@@ -2,8 +2,10 @@ package com.tlk.br.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.tlk.br.api.repositories")
 public class ApiApplication {
 
 	public static void main(String[] args) {
