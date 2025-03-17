81f53b1fa2dd6c394620d862d93f3a52de093230
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-03-17 11:26:20 -0300

    ADD: Implementado endpoint para deletar usuário pelo ID no UserController

--- src/main/java/com/tlk/br/api/controllers/UserController.java
+++ src/main/java/com/tlk/br/api/controllers/UserController.java
@@ -12,6 +12,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,5 +41,11 @@ public class UserController {
     public ResponseEntity<User> findById(@PathVariable Long id) {
         return ResponseEntity.ok(UserService.findById(id));
     }
-    
+     
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Deletar um usuário pelo ID")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        UserService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
