2d4c340ddbda017f70c5fee6fdf710730e70abcc
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-03-18 16:09:17 -0300

    ADD: Create ColaboradorDTO class with properties for collaborator details

---
+++ src/main/java/com/tlk/br/api/domain/dtos/ColaboradorDTO.java
@@ -0,0 +1,298 @@
+package com.tlk.br.api.domain.dtos;
+
+import java.util.Date;
+
+public class ColaboradorDTO {
+    private String empresa_unidade_prefixo;
+    private boolean ativo;
+    private String nome;
+    private Character sexo;
+    private Date data_nascimento;
+    private String local_nascimento;
+    private String nome_pai;
+    private String nome_mae;
+    private String estado_civil;
+    private String certidao_casamento;
+    private String cpf;
+    private String rg;
+    private String cnh;
+    private String numero_reservista;
+    private String celular;
+    private String email;
+    private String modo_trabalho;
+    private String cep;
+    private String uf;
+    private String cidade;
+    private String bairro;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String titulo_profissional;
+    private String registro_profissional;
+    private Date data_admissao;
+    private Date data_demissao;
+    private String pis;
+    private String setor;
+    private String funcao;
+    private String especialidade;
+
+    public ColaboradorDTO() {
+    }
+
+    public String getEmpresa_unidade_prefixo() {
+        return this.empresa_unidade_prefixo;
+    }
+
+    public void setEmpresa_unidade_prefixo(String empresa_unidade_prefixo) {
+        this.empresa_unidade_prefixo = empresa_unidade_prefixo;
+    }
+
+    public boolean isAtivo() {
+        return this.ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Character getSexo() {
+        return this.sexo;
+    }
+
+    public void setSexo(Character sexo) {
+        this.sexo = sexo;
+    }
+
+    public Date getData_nascimento() {
+        return this.data_nascimento;
+    }
+
+    public void setData_nascimento(Date data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+    public String getLocal_nascimento() {
+        return this.local_nascimento;
+    }
+
+    public void setLocal_nascimento(String local_nascimento) {
+        this.local_nascimento = local_nascimento;
+    }
+
+    public String getNome_pai() {
+        return this.nome_pai;
+    }
+
+    public void setNome_pai(String nome_pai) {
+        this.nome_pai = nome_pai;
+    }
+
+    public String getNome_mae() {
+        return this.nome_mae;
+    }
+
+    public void setNome_mae(String nome_mae) {
+        this.nome_mae = nome_mae;
+    }
+
+    public String getEstado_civil() {
+        return this.estado_civil;
+    }
+
+    public void setEstado_civil(String estado_civil) {
+        this.estado_civil = estado_civil;
+    }
+
+    public String getCertidao_casamento() {
+        return this.certidao_casamento;
+    }
+
+    public void setCertidao_casamento(String certidao_casamento) {
+        this.certidao_casamento = certidao_casamento;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return this.rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCnh() {
+        return this.cnh;
+    }
+
+    public void setCnh(String cnh) {
+        this.cnh = cnh;
+    }
+
+    public String getNumero_reservista() {
+        return this.numero_reservista;
+    }
+
+    public void setNumero_reservista(String numero_reservista) {
+        this.numero_reservista = numero_reservista;
+    }
+
+    public String getCelular() {
+        return this.celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getModo_trabalho() {
+        return this.modo_trabalho;
+    }
+
+    public void setModo_trabalho(String modo_trabalho) {
+        this.modo_trabalho = modo_trabalho;
+    }
+
+    public String getCep() {
+        return this.cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getUf() {
+        return this.uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getCidade() {
+        return this.cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return this.bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getLogradouro() {
+        return this.logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return this.complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getTitulo_profissional() {
+        return this.titulo_profissional;
+    }
+
+    public void setTitulo_profissional(String titulo_profissional) {
+        this.titulo_profissional = titulo_profissional;
+    }
+
+    public String getRegistro_profissional() {
+        return this.registro_profissional;
+    }
+
+    public void setRegistro_profissional(String registro_profissional) {
+        this.registro_profissional = registro_profissional;
+    }
+
+    public Date getData_admissao() {
+        return this.data_admissao;
+    }
+
+    public void setData_admissao(Date data_admissao) {
+        this.data_admissao = data_admissao;
+    }
+
+    public Date getData_demissao() {
+        return this.data_demissao;
+    }
+
+    public void setData_demissao(Date data_demissao) {
+        this.data_demissao = data_demissao;
+    }
+
+    public String getPis() {
+        return this.pis;
+    }
+
+    public void setPis(String pis) {
+        this.pis = pis;
+    }
+
+    public String getSetor() {
+        return this.setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    public String getFuncao() {
+        return this.funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+
+    public String getEspecialidade() {
+        return this.especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+}
