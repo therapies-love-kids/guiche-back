ec535f3fe101bf184b7a5a13b245fb607d666813
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-03-19 17:35:15 -0300

    ADD: Criada a entidade Responsavel para gerenciar detalhes de responsáveis

---
+++ src/main/java/com/tlk/br/api/domain/entitites/Responsavel.java
@@ -0,0 +1,10 @@
+package com.tlk.br.api.domain.entitites;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "responsavel")
+public class Responsavel {
+    
+}
