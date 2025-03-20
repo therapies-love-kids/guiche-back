9046c521f145de2a8896755c6b9c842d748ccb22
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-03-20 17:32:17 -0300

    DEL: Removida a importação de jakarta.annotation.Generated na classe User

--- src/main/java/com/tlk/br/api/domain/entitites/User.java
+++ src/main/java/com/tlk/br/api/domain/entitites/User.java
@@ -1,6 +1,6 @@
 package com.tlk.br.api.domain.entitites;
 
-import jakarta.annotation.Generated;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
