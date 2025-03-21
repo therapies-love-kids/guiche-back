7ce0f82c12d2463e9a25f8d0ba5be92c31c1f401
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-03-21 10:28:06 -0300

    ADD: Criada interface ConvenioService com métodos para gerenciar entidades Convenio

---
+++ src/main/java/com/tlk/br/api/services/ConvenioService.java
@@ -0,0 +1,12 @@
+package com.tlk.br.api.services;
+
+import com.tlk.br.api.domain.dtos.ConvenioDTO;
+import com.tlk.br.api.domain.entitites.Convenio;
+
+public interface ConvenioService {
+    Convenio save (ConvenioDTO convenioDTO);
+    Convenio update (ConvenioDTO convenioDTO);
+    Convenio findById(Long id);
+    Convenio delete (ConvenioDTO convenioDTO);
+    Convenio findByPacienteId(Long id);
+}
