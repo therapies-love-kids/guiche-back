a313953d9c8aef3feee71b111fbeb65a6eb3d148
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-03-24 17:04:27 -0300

    ADD: Implementado o controlador CaminhoController com métodos para criar, atualizar, buscar e deletar caminhos

---
+++ src/main/java/com/tlk/br/api/controllers/CaminhoController.java
@@ -0,0 +1,55 @@
+package com.tlk.br.api.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tlk.br.api.domain.dtos.CaminhoDTO;
+import com.tlk.br.api.domain.entitites.Caminho;
+import com.tlk.br.api.services.CaminhoService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@RequestMapping("/caminho")
+@Tag(name = "Caminho", description = "Caminho")
+public class CaminhoController {
+
+    private final CaminhoService caminhoService;
+
+    public CaminhoController(CaminhoService caminhoService) {
+        this.caminhoService = caminhoService;
+    }
+    
+    @PostMapping 
+    @Operation(summary = "Criar um novo caminho")
+    public ResponseEntity<Caminho> save(@RequestBody CaminhoDTO caminhoDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(caminhoService.save(caminhoDTO));
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Atualizar um caminho existente")
+    public ResponseEntity<Caminho> update(@PathVariable Long id, @RequestBody CaminhoDTO caminhoDTO) {
+        return ResponseEntity.ok(caminhoService.update(caminhoDTO));
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Buscar um caminho pelo ID")
+    public ResponseEntity<Caminho> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(caminhoService.findById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Deletar um caminho pelo ID")
+    public void delete(@PathVariable Long id) {
+        caminhoService.delete(id);
+    }
+}
