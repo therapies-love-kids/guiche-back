9a223c891c61ea860788c769440bfcb35118d8e6
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-03-24 16:06:43 -0300

    ADD: Adicionada importação da classe Optional e comentada a assinatura do método findByPacienteId na interface ContratoRepository

--- src/main/java/com/tlk/br/api/repositories/ContratoRepository.java
+++ src/main/java/com/tlk/br/api/repositories/ContratoRepository.java
@@ -1,10 +1,14 @@
 package com.tlk.br.api.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.tlk.br.api.domain.entitites.Contrato;
 
 @Repository
 public interface ContratoRepository extends JpaRepository<Contrato, Long> {
+
+    // Optional<Contrato> findByPacienteId(Long id);
 
 }
