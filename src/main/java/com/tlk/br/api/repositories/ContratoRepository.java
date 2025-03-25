03f1175e8b7bc01bb5cd2e98d9f259c9dfdb592c
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-03-25 17:10:29 -0300

    DEL: Removidos imports não utilizados na classe ContratoRepository

--- src/main/java/com/tlk/br/api/repositories/ContratoRepository.java
+++ src/main/java/com/tlk/br/api/repositories/ContratoRepository.java
@@ -1,7 +1,5 @@
 package com.tlk.br.api.repositories;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.tlk.br.api.domain.entitites.Contrato;
