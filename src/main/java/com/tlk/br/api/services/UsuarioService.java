cac8291937d84f9ad587d0713e1a51be5876899d
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-03-25 14:50:32 -0300

    ADD: Criado a interface UsuarioService para gerenciar operações relacionadas a usuários na API

---
+++ src/main/java/com/tlk/br/api/services/UsuarioService.java
@@ -0,0 +1,18 @@
+package com.tlk.br.api.services;
+
+import java.util.List;
+
+import com.tlk.br.api.domain.dtos.UsuarioDTO;
+import com.tlk.br.api.domain.entitites.Usuario;
+
+public interface UsuarioService {
+
+    boolean validateUserPassword(Long id, String password);
+    Usuario save(UsuarioDTO usuarioDTO);
+    List<Usuario> findAll();
+    Usuario findById(Long id);
+    Usuario update(Long id,UsuarioDTO usuarioDTO);
+    void delete(Long id);
+    void activateUser(Long id);
+    void deactivateUser(Long id);
+}
