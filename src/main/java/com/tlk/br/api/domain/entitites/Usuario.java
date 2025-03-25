ed0da729a3a999b137b6a067533333e9fc59b55e
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-03-25 17:10:23 -0300

    ADD: Adicionadas anotações de ID e geração automática na classe Usuario

--- src/main/java/com/tlk/br/api/domain/entitites/Usuario.java
+++ src/main/java/com/tlk/br/api/domain/entitites/Usuario.java
@@ -1,11 +1,16 @@
 package com.tlk.br.api.domain.entitites;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "usuario")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long pk;
     private Long colaborador_id;
     private boolean ativo;
