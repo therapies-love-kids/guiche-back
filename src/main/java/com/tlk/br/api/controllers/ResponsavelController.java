94e9099982cf568ddc6a6aa69d7309ea09bcafd6
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-03-25 17:10:06 -0300

    DEL: Removidos imports não utilizados no ResponsavelController

--- src/main/java/com/tlk/br/api/controllers/ResponsavelController.java
+++ src/main/java/com/tlk/br/api/controllers/ResponsavelController.java
@@ -8,19 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tlk.br.api.domain.dtos.ColaboradorDTO;
 import com.tlk.br.api.domain.dtos.ResponsavelDTO;
-import com.tlk.br.api.domain.entitites.Colaborador;
 import com.tlk.br.api.domain.entitites.Responsavel;
 import com.tlk.br.api.services.ResponsavelService;
-import com.tlk.br.api.services.UserService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.persistence.Id;
 
 @RestController
 @RequestMapping("/responsavel")
