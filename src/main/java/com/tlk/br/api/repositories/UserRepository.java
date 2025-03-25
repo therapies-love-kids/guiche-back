e5b96c06f27aa918340365627500cb45c72dcaaa
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-03-25 17:10:34 -0300

    ADD: Implementada a validação de senha do usuário no repositório UserRepository

--- src/main/java/com/tlk/br/api/repositories/UserRepository.java
+++ src/main/java/com/tlk/br/api/repositories/UserRepository.java
@@ -1,6 +1,8 @@
 package com.tlk.br.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.tlk.br.api.domain.entitites.User;
@@ -8,5 +10,8 @@ import com.tlk.br.api.domain.entitites.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM Usuario u WHERE u.id = :id AND u.senha = :senha")
+    boolean validateUserPassword(@Param("id") Long id, @Param("senha") String senha);
 
+    
 }
