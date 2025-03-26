64e985862eedd0a17ef2c85b2ee25ecf5170695f
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-03-26 17:15:37 -0300

    ADD: Adicionada configuração de CORS no UsuarioController para permitir requisições do frontend

--- src/main/java/com/tlk/br/api/controllers/UsuarioController.java
+++ src/main/java/com/tlk/br/api/controllers/UsuarioController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/usuarios")
+@CrossOrigin(origins = "http://localhost:5173")
 @Tag(name = "Usuários", description = "Usuários")
 public class UsuarioController {
     private final UsuarioService usuarioService;
