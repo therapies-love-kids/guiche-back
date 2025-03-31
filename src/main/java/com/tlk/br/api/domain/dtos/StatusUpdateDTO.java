b27d159114934f6064f0fa9efc63b13b55f2cd22
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-03-31 17:30:31 -0300

    ADD: `StatusUpdateDTO` implementado para gerenciamento de status

---
+++ src/main/java/com/tlk/br/api/domain/dtos/StatusUpdateDTO.java
@@ -0,0 +1,13 @@
+package com.tlk.br.api.domain.dtos;
+
+public class StatusUpdateDTO {
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
