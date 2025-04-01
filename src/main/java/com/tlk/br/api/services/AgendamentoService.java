315ec0d67558f2ae48e7245e7f2b5ea6428c0ecf
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-04-01 16:08:53 -0300

    ADD: Novo método para filtrar agendamentos por data e colaborador no `AgendamentoService`

--- src/main/java/com/tlk/br/api/services/AgendamentoService.java
+++ src/main/java/com/tlk/br/api/services/AgendamentoService.java
@@ -2,6 +2,8 @@ package com.tlk.br.api.services;
 
 import com.tlk.br.api.domain.entitites.Agendamento;
 import com.tlk.br.api.domain.dtos.AgendamentoDTO;
+
+import java.sql.Timestamp;
 import java.util.List;
 
 public interface AgendamentoService {
@@ -10,4 +12,5 @@ public interface AgendamentoService {
     List<Agendamento> getWaitingAgendamentos(); // Novo método para agendamentos em espera
     Agendamento save(Agendamento agendamento);
     Agendamento updateStatus(Long id, String status); // Novo método para atualizar o status
+    List<AgendamentoDTO> getAgendamentosByDateAndColaborador(Long especialistaColaboradorId, Timestamp data);
 }
