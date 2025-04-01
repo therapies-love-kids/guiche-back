462cd2de05a45dc1b9d208acd7515a0fb0352b98
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-04-01 15:35:29 -0300

    MOD: Adicionados parâmetros para filtrar agendamentos atuais e anteriores por colaborador no `AgendamentoService`

--- src/main/java/com/tlk/br/api/services/AgendamentoService.java
+++ src/main/java/com/tlk/br/api/services/AgendamentoService.java
@@ -5,8 +5,8 @@ import com.tlk.br.api.domain.dtos.AgendamentoDTO;
 import java.util.List;
 
 public interface AgendamentoService {
-    List<AgendamentoDTO> getCurrentAgendamentos();
-    List<AgendamentoDTO> getPreviousAgendamentos();
+    List<AgendamentoDTO> getCurrentAgendamentos(Long especialistaColaboradorId); // Adicionado parâmetro
+    List<AgendamentoDTO> getPreviousAgendamentos(Long especialistaColaboradorId); // Adicionado parâmetro
     List<Agendamento> getWaitingAgendamentos(); // Novo método para agendamentos em espera
     Agendamento save(Agendamento agendamento);
     Agendamento updateStatus(Long id, String status); // Novo método para atualizar o status
