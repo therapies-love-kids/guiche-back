41ddd5b0c22b890ac73c7bce104a0d79470197e9
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-04-02 13:37:48 -0300

    FIX: Corrigido o nome do pacote de entidades para `entities` no repositório `UsuarioRepository`

--- src/main/java/com/tlk/br/api/repositories/UsuarioRepository.java
+++ src/main/java/com/tlk/br/api/repositories/UsuarioRepository.java
@@ -3,7 +3,7 @@ package com.tlk.br.api.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.tlk.br.api.domain.entitites.Usuario;
+import com.tlk.br.api.domain.entities.Usuario;
 
 
 @Repository
