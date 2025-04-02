11414f8252bc08e406d9713ce0777503a2c2d0de
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-04-02 13:39:46 -0300

    FEAT: Implementado o serviço Pagamento com métodos para salvar, atualizar, buscar e deletar pagamentos

---
+++ src/main/java/com/tlk/br/api/services/impl/PagamentoServiceImpl.java
@@ -0,0 +1,43 @@
+package com.tlk.br.api.services.impl;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import com.tlk.br.api.domain.dtos.PagamentoDTO;
+import com.tlk.br.api.domain.entities.Pagamento;
+import com.tlk.br.api.repositories.PagamentoRepository;
+import com.tlk.br.api.services.PagamentoService;
+
+@Service
+public class PagamentoServiceImpl implements PagamentoService {
+
+    private final PagamentoRepository pagamentoRepository;
+
+    public PagamentoServiceImpl(PagamentoRepository pagamentoRepository) {
+        this.pagamentoRepository = pagamentoRepository;
+    }
+
+    @Override
+    public Pagamento save(PagamentoDTO pagamentoDTO) {
+        Pagamento pagamento = new Pagamento();
+        BeanUtils.copyProperties(pagamentoDTO, pagamento);
+        return pagamentoRepository.save(pagamento);
+    }
+
+    @Override
+    public Pagamento update(PagamentoDTO pagamentoDTO) {
+        Pagamento pagamento = findById(pagamentoDTO.getId());
+        BeanUtils.copyProperties(pagamentoDTO, pagamento);
+        return pagamentoRepository.save(pagamento);
+    }
+
+    @Override
+    public Pagamento findById(Long id) {
+        return pagamentoRepository.findById(id).orElseThrow();
+    }
+
+    @Override
+    public void delete(Long id) {
+        pagamentoRepository.deleteById(id);
+    }
+}
