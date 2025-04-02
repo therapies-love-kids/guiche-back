65884ae418bb02af552d7db50875b48ee6916a54
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-04-02 13:32:55 -0300

    FIX: Corrigido o import da entidade `Documento` no `DocumentoController`

--- src/main/java/com/tlk/br/api/controllers/DocumentoController.java
+++ src/main/java/com/tlk/br/api/controllers/DocumentoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tlk.br.api.domain.dtos.DocumentoDTO;
-import com.tlk.br.api.domain.entitites.Documento;
+import com.tlk.br.api.domain.entities.Documento;
 import com.tlk.br.api.services.DocumentoService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
