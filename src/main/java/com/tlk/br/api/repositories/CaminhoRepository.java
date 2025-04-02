659c0bbc419cbcce9dabaae6140213ce01b617f5
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-04-02 13:36:57 -0300

    FIX: Corrigido o nome do pacote de entidades para `entities` no repositório `CaminhoRepository`

--- src/main/java/com/tlk/br/api/repositories/CaminhoRepository.java
+++ src/main/java/com/tlk/br/api/repositories/CaminhoRepository.java
@@ -3,7 +3,7 @@ package com.tlk.br.api.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.tlk.br.api.domain.entitites.Caminho;
+import com.tlk.br.api.domain.entities.Caminho;
 
 @Repository 
 public interface CaminhoRepository extends JpaRepository<Caminho, Long> {
