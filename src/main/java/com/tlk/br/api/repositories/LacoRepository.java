4f5034c1fc4af98108a3b2b72f18d8a2731b3e73
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-04-02 13:37:25 -0300

    FIX: Corrigido o nome do pacote de entidades para `entities` no repositório `LacoRepository`

--- src/main/java/com/tlk/br/api/repositories/LacoRepository.java
+++ src/main/java/com/tlk/br/api/repositories/LacoRepository.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.tlk.br.api.domain.entitites.Laco;
+import com.tlk.br.api.domain.entities.Laco;
 
 @Repository
 public interface LacoRepository extends JpaRepository<Laco, Long> {
