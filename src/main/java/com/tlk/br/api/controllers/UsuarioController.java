e2a5e7803de9bdba3cac0af0fd6e5c04076c81a5
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-04-02 13:34:24 -0300

    FIX: Corrigido o nome do pacote da entidade `Usuario` no `UsuarioController`

--- src/main/java/com/tlk/br/api/controllers/UsuarioController.java
+++ src/main/java/com/tlk/br/api/controllers/UsuarioController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tlk.br.api.domain.dtos.UsuarioDTO;
-import com.tlk.br.api.domain.entitites.Usuario;
+import com.tlk.br.api.domain.entities.Usuario;
 import com.tlk.br.api.services.UsuarioService;
 
 import io.swagger.v3.oas.annotations.Operation;
