aa37f8e299121a1edbe750ba7a0e29ef703caac2
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-04-02 13:32:43 -0300

    FIX: Corrigido o nome do pacote da entidade `Contrato` no `ContratoController`

--- src/main/java/com/tlk/br/api/controllers/ContratoController.java
+++ src/main/java/com/tlk/br/api/controllers/ContratoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tlk.br.api.domain.dtos.ContratoDTO;
-import com.tlk.br.api.domain.entitites.Contrato;
+import com.tlk.br.api.domain.entities.Contrato;
 import com.tlk.br.api.services.ContratoService;
 
 import io.swagger.v3.oas.annotations.Operation;
