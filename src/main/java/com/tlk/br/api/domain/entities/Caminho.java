9dbddfc81f6aeea91279a29b0561287149a7a46a
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-04-02 13:35:49 -0300

    ADD: Criada a entidade `Caminho` com campos e mapeamento JPA

---
+++ src/main/java/com/tlk/br/api/domain/entities/Caminho.java
@@ -0,0 +1,87 @@
+package com.tlk.br.api.domain.entities;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "caminho")
+public class Caminho {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pk;
+    @Column (name = "usuario_id")
+    private Long usuarioId;
+    @Column (name = "data_hora_criacao")
+    private Date dataHoraCriacao;
+    private boolean ativo;
+    private String caminho;
+    private boolean editavel;
+    private boolean leitura;
+
+    public Caminho() {
+    }
+
+    public Long getPk() {
+        return this.pk;
+    }
+
+    public void setPk(Long pk) {
+        this.pk = pk;
+    }
+
+    public Long getUsuarioId() {
+        return this.usuarioId;
+    }
+
+    public void setUsuarioId(Long usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public Date getDataHoraCriacao() {
+        return this.dataHoraCriacao;
+    }
+
+    public void setDataHoraCriacao(Date dataHoraCriacao) {
+        this.dataHoraCriacao = dataHoraCriacao;
+    }
+
+    public boolean isAtivo() {
+        return this.ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public String getCaminho() {
+        return this.caminho;
+    }
+
+    public void setCaminho(String caminho) {
+        this.caminho = caminho;
+    }
+
+    public boolean isEditavel() {
+        return this.editavel;
+    }
+
+    public void setEditavel(boolean editavel) {
+        this.editavel = editavel;
+    }
+
+    public boolean isLeitura() {
+        return this.leitura;
+    }
+
+    public void setLeitura(boolean leitura) {
+        this.leitura = leitura;
+    }
+
+}
