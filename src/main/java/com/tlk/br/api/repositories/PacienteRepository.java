1faff5070c06101cf98840938fc9086c718e3324
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-04-02 13:37:33 -0300

    FIX: Corrigido o nome do pacote de entidades para `entities` no repositório `PacienteRepository`

--- src/main/java/com/tlk/br/api/repositories/PacienteRepository.java
+++ src/main/java/com/tlk/br/api/repositories/PacienteRepository.java
@@ -1,11 +1,9 @@
 package com.tlk.br.api.repositories;
 
+import com.tlk.br.api.domain.entities.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.tlk.br.api.domain.entitites.Paciente;
-
 @Repository
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
-
 }
