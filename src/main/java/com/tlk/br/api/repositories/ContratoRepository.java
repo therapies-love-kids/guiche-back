f21c69dabd92023ddafc6f40d334691853ca57a1
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-04-02 13:37:06 -0300

    FIX: Corrigido o nome do pacote de entidades para `entities` no repositório `ContratoRepository`

--- src/main/java/com/tlk/br/api/repositories/ContratoRepository.java
+++ src/main/java/com/tlk/br/api/repositories/ContratoRepository.java
@@ -2,7 +2,8 @@ package com.tlk.br.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.tlk.br.api.domain.entitites.Contrato;
+
+import com.tlk.br.api.domain.entities.Contrato;
 
 @Repository
 public interface ContratoRepository extends JpaRepository<Contrato, Long> {
