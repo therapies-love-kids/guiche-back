ef5f01dcf8abad808c49c4ce09fd4039daae5156
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-04-02 13:37:17 -0300

    FIX: Corrigido o nome do pacote de entidades para `entities` no repositório `DocumentoRepository`

--- src/main/java/com/tlk/br/api/repositories/DocumentoRepository.java
+++ src/main/java/com/tlk/br/api/repositories/DocumentoRepository.java
@@ -3,7 +3,7 @@ package com.tlk.br.api.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.tlk.br.api.domain.entitites.Documento;
+import com.tlk.br.api.domain.entities.Documento;
 
 @Repository
 public interface DocumentoRepository extends JpaRepository<Documento, Long> {
