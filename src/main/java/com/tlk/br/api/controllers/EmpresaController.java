9469d48bc048ec2ffeebca645a640f9458435bb8
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-04-02 13:33:01 -0300

    FIX: Corrigido o nome do pacote da entidade `Empresa` no `EmpresaController`

--- src/main/java/com/tlk/br/api/controllers/EmpresaController.java
+++ src/main/java/com/tlk/br/api/controllers/EmpresaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tlk.br.api.domain.dtos.EmpresaDTO;
-import com.tlk.br.api.domain.entitites.Empresa;
+import com.tlk.br.api.domain.entities.Empresa;
 import com.tlk.br.api.services.EmpresaService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
