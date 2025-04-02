6d23341e3b80efe55aafa36292cfb5b810033202
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-04-02 13:37:39 -0300

    FIX: Corrigido o nome do pacote de entidades para `entities` no repositório `PagamentoRepository`

--- src/main/java/com/tlk/br/api/repositories/PagamentoRepository.java
+++ src/main/java/com/tlk/br/api/repositories/PagamentoRepository.java
@@ -3,7 +3,7 @@ package com.tlk.br.api.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.tlk.br.api.domain.entitites.Pagamento;
+import com.tlk.br.api.domain.entities.Pagamento;
 
 @Repository
 public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
