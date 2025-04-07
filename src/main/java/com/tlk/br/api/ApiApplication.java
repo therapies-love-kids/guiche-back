b477e724b91ec4c7e5374431b4254d7c129f15d1
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-04-07 16:23:09 -0300

    FEAT: Adicionado suporte a CORS para permitir requisições do frontend

--- src/main/java/com/tlk/br/api/ApiApplication.java
+++ src/main/java/com/tlk/br/api/ApiApplication.java
@@ -2,7 +2,10 @@ package com.tlk.br.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.tlk.br.api.repositories")
@@ -11,5 +14,20 @@ public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
+
+	 @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173") // Origem do frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Todos os headers permitidos
+                        .allowCredentials(true) // Permite credenciais (cookies, etc.)
+                        .maxAge(3600); // Cache das opções CORS por 1 hora
+            }
+        };
+    }
 
 }
