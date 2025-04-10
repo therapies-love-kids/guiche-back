7c09899411fb750ba7da92243f9e467a5b60f09b
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-04-10 10:52:01 -0300

    MOD: Interface `ConvenioService` refatorada para atualizar assinaturas de métodos e melhorar a consistência de tipos

--- src/main/java/com/tlk/br/api/services/ConvenioService.java
+++ src/main/java/com/tlk/br/api/services/ConvenioService.java
@@ -1,12 +1,13 @@
 package com.tlk.br.api.services;
 
 import com.tlk.br.api.domain.dtos.ConvenioDTO;
-import com.tlk.br.api.domain.entities.Convenio;
+import java.util.List;
 
 public interface ConvenioService {
-    Convenio save (ConvenioDTO convenioDTO);
-    Convenio update (ConvenioDTO convenioDTO);
-    Convenio findById(Long id);
-    Convenio delete (ConvenioDTO convenioDTO);
-    Convenio findByPacienteId(Long id);
+
+    ConvenioDTO save(ConvenioDTO convenioDTO);
+    ConvenioDTO update(Integer pk, ConvenioDTO convenioDTO);
+    ConvenioDTO findByPk(Integer pk);
+    void delete(Integer pk);
+    List<ConvenioDTO> findAll();
 }
