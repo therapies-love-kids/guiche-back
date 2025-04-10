cd6721f140ad4eb03dff89919f124fe82ae5a1fa
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-04-10 10:50:01 -0300

    MOD: Entidade `Convenio` refatorada para simplificação, remoção de código boilerplate e uso do Lombok

--- src/main/java/com/tlk/br/api/domain/entities/Convenio.java
+++ src/main/java/com/tlk/br/api/domain/entities/Convenio.java
@@ -1,56 +1,23 @@
 package com.tlk.br.api.domain.entities;
 
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
 @Table(name = "convenio")
+@Data
 public class Convenio {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long pk;
-    private boolean ativo;
+    private Integer pk;
+
+    @Column(nullable = false)
+    private Boolean ativo = true;
+
+    @Column(length = 64, nullable = false)
     private String nome;
-    private String nome_curto;
 
-    
-    public Convenio() {
-    }
-    
-    public Long getPk() {
-        return this.pk;
-    }
-
-    public void setPk(Long pk) {
-        this.pk = pk;
-    }
-
-    public boolean isAtivo() {
-        return this.ativo;
-    }
-
-    public void setAtivo(boolean ativo) {
-        this.ativo = ativo;
-    }
-
-    public String getNome() {
-        return this.nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getNome_curto() {
-        return this.nome_curto;
-    }
-
-    public void setNome_curto(String nome_curto) {
-        this.nome_curto = nome_curto;
-    }
-
+    @Column(name = "nome_curto", length = 16, nullable = false)
+    private String nomeCurto;
 }
