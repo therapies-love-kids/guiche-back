c7554bfa0216d5eb78f2842bf91477502efef9e4
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-04-10 10:51:36 -0300

    MOD: `ConvenioRepository` refatorado para alterar o tipo de ID de `Long` para `Integer`, garantindo consistência com a definição da entidade

--- src/main/java/com/tlk/br/api/repositories/ConvenioRepository.java
+++ src/main/java/com/tlk/br/api/repositories/ConvenioRepository.java
@@ -1,10 +1,9 @@
 package com.tlk.br.api.repositories;
 
+import com.tlk.br.api.domain.entities.Convenio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.tlk.br.api.domain.entities.Convenio;
 @Repository
-public interface ConvenioRepository extends JpaRepository<Convenio, Long> {
-
+public interface ConvenioRepository extends JpaRepository<Convenio, Integer> {
 }
